98661e7d6926413ebb605cb65da738193377b4bc
maybe-skiFF <den.skiff190@gmail.com>
2024-11-15 00:14:06 +0300

    feat: add links to header to navigate between users and posts lists

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import { HeaderMenu } from '../HeaderMenu/HeaderMenu';
 import { Logo } from '../Logo/Logo';
 import styles from './Header.module.css';
@@ -7,6 +8,14 @@ const Header = () => {
     <div className={styles.headerContainer}>
       <div className={styles.headerWrapper}>
         <Logo />
+        <div className={styles.headerLinkWrapper}>
+          <Link className={styles.headerLink} to="/">
+            Users
+          </Link>
+          <Link className={styles.headerLink} to="/posts">
+            Posts
+          </Link>
+        </div>
         <HeaderMenu />
       </div>
     </div>
